28d57e442ce80c8d51e90499bd5b8112b15e8bd7
Zedhugh Chen <zedhugh@gmail.com>
2018-03-14 13:22:00 +0800

    better idle delay for which-key.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -9,7 +9,10 @@
 (require-package 'window-numbering)
 (require-package 'smooth-scrolling)
 
+;; which-key
 (which-key-mode)
+(setq which-key-idle-delay 0.4)
+
 (window-numbering-mode)
 (smooth-scrolling-mode)
 

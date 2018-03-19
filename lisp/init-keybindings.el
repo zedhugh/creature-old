6c86b7f18ca81cd0ea35416a680429fc34658606
Zedhugh Chen <zedhugh@gmail.com>
2018-03-19 22:58:20 +0800

    add keybindings for clipboard.

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -9,6 +9,8 @@
 (global-set-key (kbd "C-M-\\") 'creature/indent-region-or-buffer)
 (global-set-key (kbd "C-w") 'creature/backward-kill-word-or-region)
 (evil-leader/set-key "fi" 'creature/open-init-file)
+(global-set-key (kbd "s-v") 'clipboard-yank)
+(global-set-key (kbd "s-c") 'clipboard-kill-ring-save)
 
 (provide 'init-keybindings)
 ;;; init-keybindings.el ends here

4fdf2ef37941ae331c8c58266e9ffb8969cb9dfa
Zedhugh Chen <zedhugh@gmail.com>
2018-04-03 01:08:24 +0800

    init time.

--- init.el
+++ init.el
@@ -14,6 +14,8 @@
 ;;; Code:
 ;; (package-initialize)
 
+(setq start-init-time (current-time))
+
 (defconst creature-dir
   (file-name-directory (or load-file-name buffer-file-name))
   "Root directory of creature.")
@@ -33,10 +35,17 @@
 (require 'init-org)
 (require 'init-keybindings)
 
+(setq end-init-time (current-time))
+
 ;; load custom file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+(setq finial-init-time (current-time))
+
+(float-time (time-subtract end-init-time start-init-time))
+(float-time (time-subtract finial-init-time start-init-time))
 
 ;; (provide 'init)
 ;;; init.el ends here

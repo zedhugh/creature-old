90b54e10c52f4043d70086ae906d1dc6a76f8068
Zedhugh Chen <zedhugh@gmail.com>
2018-04-20 01:37:21 +0800

    add a function to install package.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -7,8 +7,18 @@
 (if (< emacs-major-version 27)
     (package-initialize)
   (unless (file-exists-p
-	    (expand-file-name "elpa" user-emacs-directory))
+           (expand-file-name "elpa" user-emacs-directory))
     (package-initialize)))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Ask elpa to install given PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
 
 (setq package-selected-packages nil)
 

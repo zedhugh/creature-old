1af1ceb14c15a6d8e63b1eda8647a75236aef56a
Zedhugh Chen <zedhugh@gmail.com>
2019-10-17 01:15:19 +0800

    don't indent by lsp server.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -12,6 +12,7 @@
 (with-eval-after-load 'lsp-mode
   (setq lsp-prefer-flymake nil)
   (setq lsp-auto-guess-root t)
+  (setq lsp-enable-indentation nil)
   (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact"))
   (add-hook 'lsp-mode-hook #'lsp-ui-mode))
 

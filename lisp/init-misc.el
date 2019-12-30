4d6991ece47048cc81f5a23e3dfd0f6cfa127c8f
Zedhugh Chen <zedhugh@gmail.com>
2019-12-30 20:45:08 +0800

    optimize `creature/kill-gpg-agent'

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -103,10 +103,9 @@
 
 ;; gpg config
 (defun creature/kill-gpg-agent ()
-  "Kill `gpg-agent' for security when Emacs be killed.
-This works only when `pinentry-mode' is running."
-  (when (and pinentry--server-process
-             (boundp 'epg-gpgconf-program))
+  "Kill `gpg-agent' for security when Emacs be killed."
+  (when (and (bound-and-true-p epg-gpgconf-program)
+             (executable-find epg-gpgconf-program))
     (start-process "" nil epg-gpgconf-program "--kill" "gpg-agent")))
 (add-hook 'kill-emacs-hook #'creature/kill-gpg-agent)
 

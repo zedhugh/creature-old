c322fd231cc59695794ed2fc826378ef9d6463d8
Zedhugh Chen <zedhugh@gmail.com>
2020-12-17 01:55:28 +0800

    change lsp-setup

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -50,12 +50,12 @@
   "Setup lsp in which `major-mode' in `creature/lsp-setup-modes'."
   (when (and
          (buffer-file-name)
-         (member major-mode creature/lsp-setup-modes))
-    (unless (featurep 'tramp)
-      (require 'tramp))
-    (when (not (tramp-tramp-file-p (buffer-file-name)))
-      (lsp-deferred)
-      (lsp-diagnostics-mode)))
+         (member major-mode creature/lsp-setup-modes)
+         (if (featurep 'tramp)
+             (not (tramp-tramp-file-p (buffer-file-name)))
+           t))
+    (lsp-deferred)
+    (lsp-diagnostics-mode))
   (when (derived-mode-p 'css-mode 'scss-mode 'less-css-mode)
     (setq-local lsp-overlay-document-color-char "")))
 
